14c6819f9286e39443feb68641602eae8da6b186
Amitai Blickstein <blickstein@gmail.com>
2022-03-11 14:30:12 -0500

    Adds PageViewController

---
+++ JWPageControllerDemo/ViewControllers/PageViewController.swift
@@ -0,0 +1,12 @@
+//
+//  PageViewController.swift
+//  JWPageControllerDemo
+//
+//  Created by Amitai Blickstein on 3/10/22.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController {
+
+}

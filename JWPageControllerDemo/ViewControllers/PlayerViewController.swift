bab2afdc7ca6e59888d8588ed76f9be17735a5a2
Amitai Blickstein <blickstein@gmail.com>
2022-03-11 14:33:47 -0500

    Adds PlayerViewController

---
+++ JWPageControllerDemo/ViewControllers/PlayerViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerViewController.swift
+//  JWPageControllerDemo
+//
+//  Created by Amitai Blickstein on 3/10/22.
+//
+
+import UIKit
+import JWPlayerKit
+
+class PlayerViewController: JWPlayerViewController {
+    
+}
